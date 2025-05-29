5b6d4be59a978f4cb43dc98541aa45315020507e
Carl Reyes <reyes.carlj@gmail.com>
2025-05-29 16:48:56 -0400

    updated theme toggle css

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_QhtL1 from "../../assets/old-course/1-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md";
+import __ASTRO_IMAGE_IMPORT_ZG9rQN from "../../assets/old-course/first-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md";
 import __ASTRO_IMAGE_IMPORT_Z2nEKWc from "../../assets/old-course/17-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md";
 import __ASTRO_IMAGE_IMPORT_165LMt from "../../assets/old-course/finished.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md";
-import __ASTRO_IMAGE_IMPORT_ZG9rQN from "../../assets/old-course/first-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md";
-export default new Map([["../../assets/old-course/1-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md", __ASTRO_IMAGE_IMPORT_QhtL1], ["../../assets/old-course/17-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md", __ASTRO_IMAGE_IMPORT_Z2nEKWc], ["../../assets/old-course/finished.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md", __ASTRO_IMAGE_IMPORT_165LMt], ["../../assets/old-course/first-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md", __ASTRO_IMAGE_IMPORT_ZG9rQN]]);
+export default new Map([["../../assets/old-course/first-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md", __ASTRO_IMAGE_IMPORT_ZG9rQN], ["../../assets/old-course/17-tee.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md", __ASTRO_IMAGE_IMPORT_Z2nEKWc], ["../../assets/old-course/finished.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-old-course.md", __ASTRO_IMAGE_IMPORT_165LMt]]);
 		
